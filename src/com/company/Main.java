0548e190f150ba16a78180fdfbec093aa1b0f3b7
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-09-08 23:17:35 +0200

    Adds Upp och Ner game

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -31,7 +31,7 @@ public class Main {
                     invalidGame = true;
                 }
             }
-            if (selectedGame > 4 || selectedGame == 0) {
+            if (selectedGame > 4 || "0".equals(input)) {
                 invalidGame = true;
                 displayOptions(true);
             }
@@ -40,24 +40,28 @@ public class Main {
 
 
         switch (selectedGame) {
-            case 1, 2 -> UpAndDown.play();
-            default -> displayOptions(true);
+            case 1, 2 -> UpAndDown.start();
 
         }
+
+        //Todo: när körningen är klar ska menyn åter skrivas ut på skärmen så att ett nytt val kan göras => alltså
+        // spelet avslutas när man tryckt "e"
+
+        // place entire game in a separate method ?
     }
 
     public static void displayOptions(boolean invalid) {
         if (invalid)
             System.out.println("Ogiltigt val, försök igen!\n");
         System.out.println(
-                """           
-                        Vänligen skriv ett nummer för att välja ett spel eller tryck "e" för att avsluta.
-                        1. Upp och Ner
-                        2. Min Max
-                        3. Sten Sax Påse
-                        4. Ordning och reda
-                        e. Avsluta
-                        """
+            """           
+            Vänligen skriv ett nummer för att välja ett spel eller tryck "e" för att avsluta.
+            1. Upp och Ner
+            2. Min Max
+            3. Sten Sax Påse
+            4. Ordning och reda
+            e. Avsluta
+            """
         );
     }
 }
--- src/com/company/games/UpAndDown.java
+++ src/com/company/games/UpAndDown.java
@@ -1,9 +1,50 @@
 package com.company.games;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class UpAndDown {
     //Todo: is the main method needed?
 
-    public static  void play() {
-        System.out.println("Välkommen till upp och ner");
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        start();
     }
+
+    public static void start() {
+        System.out.println(
+            """
+            Välkommen till upp och ner!
+            
+            Instruktioner:
+             - Skriv in ett antal ord och tryck ENTER mellan varje ord.
+             - Skriv END när du är klar.
+            """
+        );
+
+        play();
+        
+    }
+
+    public static void play() {
+        ArrayList<String> words = new ArrayList<String>();
+        boolean nextWord = true;        //inputting, typing, addWord, addingWords
+
+        while (nextWord) {
+            System.out.println("Skriv ett ord...");
+            String input = scanner.nextLine();
+
+            if("END".equals(input))
+                nextWord = false;
+            else
+                words.add(input);
+        }
+
+        System.out.println("\nDina ord i omvänd ordning:");
+        for (int i = words.size(); i > 0; i--) {
+            System.out.println(words.get(i - 1));
+        }
+    }
+
 }
