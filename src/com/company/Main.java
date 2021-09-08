5cdd033fa3cb9a610edb47e0bb9c02d0766b57b4
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-09-08 18:02:20 +0200

    Handles invalid options

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,63 @@
 package com.company;
 
+import com.company.games.UpAndDown;
+
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String gameOptions = """           
-                Vänligen skriv ett nummer för att välja ett spel eller tryck "e" för att avsluta.
-                1. Upp och Ner
-                2. Min Max
-                3. Sten Sax Påse
-                4. Ordning och reda
-                e. Avsluta
-                """;
+        boolean invalidGame;
+        int selectedGame;
+        String input;
 
         System.out.println("\nVälkommen till Det Roliga Spelet!!\n");
-        System.out.println(gameOptions);
+        displayOptions(false);
 
-        String input = scanner.nextLine();
 
-        int selectedGame = 0;
-
-        if ("e".equals(input.toLowerCase())) {
-            System.out.println("Vi ses nästa gång!");
-        } else {
-            try {
-                selectedGame = Integer.parseInt(input);
-            } catch (NumberFormatException ignored) {
-                System.out.println("Ogiltigt val, försök igen!\n");
-                System.out.println(gameOptions);
+        do {
+            selectedGame = 0;
+            input = scanner.nextLine();
+            if ("e".equalsIgnoreCase(input)) {
+                invalidGame = false;
+                System.out.println("Vi ses nästa gång!");
+            } else {
+                try {
+                    invalidGame = false;
+                    selectedGame = Integer.parseInt(input);
+                } catch (NumberFormatException ignored) {
+                    displayOptions(true);
+                    invalidGame = true;
+                }
             }
+            if (selectedGame > 4 || selectedGame == 0) {
+                invalidGame = true;
+                displayOptions(true);
+            }
+
+        } while (invalidGame);
+
+
+        switch (selectedGame) {
+            case 1, 2 -> UpAndDown.play();
+            default -> displayOptions(true);
+
         }
+    }
 
-        //use switch for both scenarios
-        //handle 4 valid numbers
-
-        //handle invalid number -> 0 or greater than 4  => request correct input
-
-        
-//        switch (input) {
-//            case Integer.parseInt(input) -> System.out.println("integer");
-//        }
+    public static void displayOptions(boolean invalid) {
+        if (invalid)
+            System.out.println("Ogiltigt val, försök igen!\n");
+        System.out.println(
+                """           
+                        Vänligen skriv ett nummer för att välja ett spel eller tryck "e" för att avsluta.
+                        1. Upp och Ner
+                        2. Min Max
+                        3. Sten Sax Påse
+                        4. Ordning och reda
+                        e. Avsluta
+                        """
+        );
     }
 }
---
+++ src/com/company/games/UpAndDown.java
@@ -0,0 +1,9 @@
+package com.company.games;
+
+public class UpAndDown {
+    //Todo: is the main method needed?
+
+    public static  void play() {
+        System.out.println("Välkommen till upp och ner");
+    }
+}
