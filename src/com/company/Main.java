5fd3467bab2e5471fae89795e2b80ada7b5e6c24
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-09-08 12:02:40 +0200

    Adds meny

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(
+            """
+            Välkommen till Det Roliga Spelet!!
+            
+            Skriv ett nummer för att välja ett spel eller tryck "e" för att avsluta.
+            1. Upp och Ner
+            2. Min Max
+            3. Sten Sax Påse
+            4. Ordning och reda
+            e. Avsluta
+            """
+        );
     }
 }
